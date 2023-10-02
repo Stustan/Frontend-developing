b67c5932ea965da87d2ef509acf8ee7c97f75e9c
Stustan <coreyes@ya.ru>
2023-10-02 23:04:41 +0300

    Module 2 (Notes)
    02.10.23

--- Vue.js/2 module/app.js
+++ Vue.js/2 module/app.js
@@ -1,9 +1,42 @@
 const App = {
     data () {
         return{
-            title: 'Notes'
-        }
-    }
-}
+            title: 'Notes',
+            input: {
+                value: '',
+                placeholder: 'Type your note',
+            },
+            notes: ["task 1", "task2"],
+        };
+    },
+    mounted() {
+        this.getNotes();
+    },
+    watch: {
+        notes: {
+            handler(updatedList){
+                localStorage.setItem('notes', JSON.stringify(updatedList));
+            },
+            deep: true,
+        },
+    },
+    methods: {
+        getNotes () {
+            const localNotes = localStorage.getItem('notes');
+            if(localNotes){
+                this.notes = JSON.parse(localNotes);
+            };
+        },
+        onSubmit () {
+            this.notes.push(this.input.value);
+            this.input.value = '';
+        },
+        remove (index) {
+            this.notes.splice(index, 1);
+
+        },
+    },
+
+};
 
 Vue.createApp(App).mount("#app");
