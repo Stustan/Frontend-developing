70a0193c745fd5d2b0d967d11166c9c53db3a40c
Stustan <coreyes@ya.ru>
2023-11-30 23:38:28 +0300

    Notes on vue

---
+++ Vue.js/3_module/vue-cli-template/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <div class="wrapper-content">
+      <div class="container">
+        <h1>hello</h1>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+{
+  aliases: [
+    {'@' : '/src/'},
+    {'~' : '/'},
+  ]
+}
+
+<script>
+import Header from "@/components/TheHeader.vue";
+import Footer from "@/components/TheFooter.vue";
+
+export default {
+  components: {
+    Header, 
+    Footer
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ Vue.js/3_module/vue-cli-template/src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div class="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <a class="navbar-logo" href="/">{{title}}</a>
+          <ul class="navbar-list">
+            <li class="navbar-item" v-for="link in links" :key="link.alias">
+              <a class="navbar-link" :href="link.url">{{
+                link.title
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import {app, links_array} from '@/_config'
+export default {
+  data () {
+    return {
+      title: app.title,
+      links: links_array,
+    };
+  },
+};
+</script>
