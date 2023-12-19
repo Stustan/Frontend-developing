26e1e5885841925cf0e7a3f2099c84a7d17c6a32
Stustan <coreyes@ya.ru>
2023-12-19 19:36:21 +0300

    changed config

--- src/_config.js
+++ src/_config.js
@@ -1,13 +1,5 @@
-export const process = {
-    dev: true,
-};
-
-export const site = {
-    home: process.dev ? "http://localhost:8080/" : "https://app.tocode.ru/",
-};
-
 export const app = {
-    title: "Template test",
+    title: "Clash of clans",
 };
 
 export const links_array = [
