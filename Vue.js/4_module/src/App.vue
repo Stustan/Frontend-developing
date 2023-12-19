2312665cbd17eaf41d4397f9d6dc3e1489bfc4df
Stustan <coreyes@ya.ru>
2023-12-19 19:07:59 +0300

    4 module

---
+++ Vue.js/4_module/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <div class="wrapper-content">
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+{
+  aliases: [
+    {'@' : '/src/'},
+    {'~' : '/'},
+  ]
+}
+
+<script>
+import Header from "@/components/TheHeader.vue";
+import Footer from "@/components/TheFooter.vue";
+
+export default {
+  components: {
+    Header, 
+    Footer
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
